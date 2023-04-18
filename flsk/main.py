2d6d5b3dcf5e5f0f45f067b85f0504621871a2f4
Dolkart <RDolkart@it-one.ru>
2023-04-18 22:51:15 +0300

    Add Regina's signature

--- flsk/main.py
+++ flsk/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'Hello World. It's Regina'
 
 if __name__ == "__main__":
     app.run(host = p_host, port = p_port)
